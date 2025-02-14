929f61d7574e4cbc322c8f81bff101f3f344f118
QuickScythe <QuickScythe@gmail.com>
2025-02-14 02:17:21 -0600

    better test

--- src/test/java/me/quickscythe/quipt/minecraft/tests/MinecraftTests.java
+++ src/test/java/me/quickscythe/quipt/minecraft/tests/MinecraftTests.java
@@ -10,7 +10,7 @@ public class MinecraftTests {
 
     @Test
     void testMinecraftServerPlayer() {
-        MinecraftServer server = new MinecraftServer("127.0.0.1", "secret", "http://localhost:8080/api/server_status/");
+        MinecraftServer server = new MinecraftServer("127.0.0.1", "secret", "https://quipt-api.azurewebsites.net/api/server_status/");
         MinecraftServerPlayer player = server.getPlayer(UUID.fromString("60191757-427b-421e-bee0-399465d7e852"));
         System.out.println(player.name());
     }
